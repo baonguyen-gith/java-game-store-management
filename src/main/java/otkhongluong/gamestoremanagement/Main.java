a52cfa26b9c965624a602c2c33e95b90ba05118c
Nguyễn Gia Bảo <24520166@gm.uit.edu.vn>
2026-04-19 01:21:22 +0700

    Add Maven Project

---
+++ src/main/java/otkhongluong/gamestoremanagement/Main.java
@@ -0,0 +1,7 @@
+package otkhongluong.gamestoremanagement;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Game Store Management System Started");
+    }   
+}
