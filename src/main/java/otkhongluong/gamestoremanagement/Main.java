ec042b2fe1d15ccb76bfa4abc83723052f5e22b9
ThanhAn-jogging <tranthanhan26082006@gmail.com>
2026-04-20 13:28:22 +0700

    test

--- src/main/java/otkhongluong/gamestoremanagement/Main.java
+++ src/main/java/otkhongluong/gamestoremanagement/Main.java
@@ -3,5 +3,7 @@ package otkhongluong.gamestoremanagement;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Game Store Management System Started");
+
+        System.out.println("meo moe meo");
     }   
 }
