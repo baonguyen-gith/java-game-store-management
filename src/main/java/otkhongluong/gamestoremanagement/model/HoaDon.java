60a05baa06bfec88922e1a6df22d7288d75cc7cb
hoangphuc-uit <hoangphucnhqn@gmail.com>
2026-04-20 22:06:27 +0700

    Create HoaDon.java

---
+++ src/main/java/otkhongluong/gamestoremanagement/model/HoaDon.java
@@ -0,0 +1,14 @@
+package otkhongluong.gamestoremanagement.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class HoaDon {
+    private String maHD;
+    private String maKH;
+    private LocalDateTime ngayLap;
+    private BigDecimal tongTien;
+
+    // Constructors, Getters and Setters
+}
