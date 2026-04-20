8519f9351fca50bd4a4dee05fea713ed093c356d
hoangphuc-uit <hoangphucnhqn@gmail.com>
2026-04-20 22:04:33 +0700

    Create Game.java

---
+++ src/main/java/otkhongluong/gamestoremanagement/model/Game.java
@@ -0,0 +1,14 @@
+package otkhongluong.gamestoremanagement.model;
+
+import java.math.BigDecimal;
+
+public class Game {
+    private String maGame;
+    private String tenGame;
+    private String theLoai;
+    private String nenTang;
+    private BigDecimal giaBan;
+    private BigDecimal giaThueNgay;
+
+    // Constructors, Getters and Setters
+}
