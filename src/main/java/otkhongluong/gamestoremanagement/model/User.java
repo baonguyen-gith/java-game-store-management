368f55b9cfca05df21fd7469aa9e9fc2f401720a
hoangphuc-uit <hoangphucnhqn@gmail.com>
2026-04-20 22:07:41 +0700

    Create User.java

---
+++ src/main/java/otkhongluong/gamestoremanagement/model/User.java
@@ -0,0 +1,10 @@
+package otkhongluong.gamestoremanagement.model;
+
+public class User {
+    private String username;
+    private String password;
+    private int maRole;
+    private String hoTen;
+
+    // Constructors, Getters and Setters
+}
