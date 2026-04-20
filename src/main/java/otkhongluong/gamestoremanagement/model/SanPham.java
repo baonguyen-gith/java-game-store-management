b5cf4a21f2723ae315703f8c2398230bb6b09282
hoangphuc-uit <hoangphucnhqn@gmail.com>
2026-04-20 22:05:01 +0700

    Create SanPham.java

---
+++ src/main/java/otkhongluong/gamestoremanagement/model/SanPham.java
@@ -0,0 +1,12 @@
+package otkhongluong.gamestoremanagement.model;
+
+import java.math.BigDecimal;
+
+public class SanPham {
+    private String maSP;
+    private String maGame;
+    private BigDecimal giaBan;
+    private String loaiSP; // "CD" hoặc "ROM"
+
+    // Constructors, Getters and Setters
+}
