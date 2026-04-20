0fc325a369ad997e155d7427513612d35f295ca9
hoangphuc-uit <hoangphucnhqn@gmail.com>
2026-04-20 22:06:52 +0700

    Create PhieuThue.java

---
+++ src/main/java/otkhongluong/gamestoremanagement/model/PhieuThue.java
@@ -0,0 +1,16 @@
+package otkhongluong.gamestoremanagement.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class PhieuThue {
+    private String maPT;
+    private String maKH;
+    private LocalDateTime ngayThue;
+    private LocalDateTime ngayTraDuKien;
+    private LocalDateTime ngayTraThucTe;
+    private BigDecimal tienCoc;
+    private BigDecimal tienPhat;
+
+    // Constructors, Getters and Setters
+}
