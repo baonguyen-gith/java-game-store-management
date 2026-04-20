ffc09c2e71fe33fc562ab086734febb8b3872afa
hoangphuc-uit <hoangphucnhqn@gmail.com>
2026-04-20 21:41:30 +0700

    Create KhachHang.java

---
+++ src/main/java/otkhongluong/gamestoremanagement/model/KhachHang.java
@@ -0,0 +1,10 @@
+package otkhongluong.gamestoremanagement.model;
+
+public class KhachHang {
+    private String maKH;
+    private String hoTen;
+    private String sdt;
+    private String email;
+    private String diaChi;
+    // Constructors, Getters and Setters
+}
