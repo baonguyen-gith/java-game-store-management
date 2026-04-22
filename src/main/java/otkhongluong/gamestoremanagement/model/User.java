6e70bd57c4d5f21b9466eb728e59d026e776079b
hoangphuc-uit <hoangphucnhqn@gmail.com>
2026-04-22 08:14:43 +0700

    User.java

--- src/main/java/otkhongluong/gamestoremanagement/model/User.java
+++ src/main/java/otkhongluong/gamestoremanagement/model/User.java
@@ -1,10 +1,29 @@
 package otkhongluong.gamestoremanagement.model;
 
 public class User {
-    private String username;
-    private String password;
-    private int maRole;
-    private String hoTen;
+    private int MaUser;
+    private String Username;
+    private String Password;
+    private int MaRole;
 
-    // Constructors, Getters and Setters
+    public User() {}
+
+    public User(int MaUser, String Username, String Password, int MaRole) {
+        this.MaUser = MaUser;
+        this.Username = Username;
+        this.Password = Password;
+        this.MaRole = MaRole;
+    }
+
+    public int getMaUser() { return MaUser; }
+    public void setMaUser(int MaUser) { this.MaUser = MaUser; }
+
+    public String getUsername() { return Username; }
+    public void setUsername(String Username) { this.Username = Username; }
+
+    public String getPassword() { return Password; }
+    public void setPassword(String Password) { this.Password = Password; }
+
+    public int getMaRole() { return MaRole; }
+    public void setMaRole(int MaRole) { this.MaRole = MaRole; }
 }
