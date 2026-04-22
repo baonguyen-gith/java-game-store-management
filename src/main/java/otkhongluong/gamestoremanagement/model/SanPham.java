5e41bc434085988ca698f6bafd1986a73341735f
hoangphuc-uit <hoangphucnhqn@gmail.com>
2026-04-22 08:13:04 +0700

    SanPham.java

--- src/main/java/otkhongluong/gamestoremanagement/model/SanPham.java
+++ src/main/java/otkhongluong/gamestoremanagement/model/SanPham.java
@@ -1,12 +1,29 @@
 package otkhongluong.gamestoremanagement.model;
 
-import java.math.BigDecimal;
-
 public class SanPham {
-    private String maSP;
-    private String maGame;
-    private BigDecimal giaBan;
-    private String loaiSP; // "CD" hoặc "ROM"
+    private int MaSP;
+    private int MaGame;
+    private double GiaBan;
 
-    // Constructors, Getters and Setters
+    public SanPham() {}
+
+    public SanPham(int MaSP, int MaGame, double GiaBan) {
+        this.MaSP = MaSP;
+        this.MaGame = MaGame;
+        this.GiaBan = GiaBan;
+    }
+
+    public int getMaSP() { return MaSP; }
+    public void setMaSP(int MaSP) { this.MaSP = MaSP; }
+
+    public int getMaGame() { return MaGame; }
+    public void setMaGame(int MaGame) { this.MaGame = MaGame; }
+
+    public double getGiaBan() { return GiaBan; }
+    public void setGiaBan(double GiaBan) { this.GiaBan = GiaBan; }
+
+    @Override
+    public String toString() {
+        return "SanPham{" + "MaSP=" + MaSP + ", MaGame=" + MaGame + ", GiaBan=" + GiaBan + '}';
+    }
 }
